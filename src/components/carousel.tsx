2fcccfd2c2f41be274cbe92ddf9fd7af7c094b70
Guto Moraes <joseaugusto.teo@outlook.com.br>
2026-04-14 18:52:51 -0300

    Mudança na estilização do componente Carrossel estilo Instragam do CSS 'puro' para classes do Tailwind

--- src/components/carousel.tsx
+++ src/components/carousel.tsx
@@ -2,46 +2,46 @@
 
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
-// import { cn } from "@/lib/utils";
+import { cn } from "@/lib/utils";
 import image_one from "@/assets/images/experiments/1.webp";
 import image_two from "@/assets/images/experiments/2.webp";
 import image_three from "@/assets/images/experiments/3.webp";
 import logo_guto from "@/assets/images/builtby.webp";
-import logo_cbc from "@/assets/images/logo192.png";
+import logo_cbc from "@/assets/images/logo-icon.webp";
 
 type StoriesTypes = {
-  profileImage: string;
+  profileIconSrc: string;
   profileName: string;
   title: string[];
-  link: string;
+  linkSrc: string;
   linkLabel: string;
-  storyImage: string;
+  imageSrc: string;
 };
 
 const stories: StoriesTypes[] = [
   {
-    profileImage: logo_cbc,
-    profileName: "Caminhos",
+    profileIconSrc: logo_cbc,
+    profileName: "Caminhos do Brasil Central",
     title: ["História", "Memória", "e Patrimônio"],
-    link: "https://caminhosdobrasilcentral.com",
-    linkLabel: "Saiba mais",
-    storyImage: image_one,
+    linkSrc: "https://caminhosdobrasilcentral.com",
+    linkLabel: "Descubra",
+    imageSrc: image_one,
   },
   {
-    profileImage: logo_guto,
+    profileIconSrc: logo_guto,
     profileName: "Guto Moraes",
     title: ["Teologia", "História", "e Front-end"],
     linkLabel: "Saiba mais",
-    link: "https://github.com/guto-moraes",
-    storyImage: image_two,
+    linkSrc: "https://github.com/guto-moraes",
+    imageSrc: image_two,
   },
   {
-    profileImage: logo_cbc,
-    profileName: "Caminhos",
+    profileIconSrc: logo_cbc,
+    profileName: "Caminhos do Brasil Central",
     title: ["Expedição Roncador-Xingu", "Fundação Brasil Central", "e Vale do Araguaia"],
     linkLabel: "Saiba mais",
-    link: "https://caminhosdobrasilcentral.com",
-    storyImage: image_three,
+    linkSrc: "https://caminhosdobrasilcentral.com",
+    imageSrc: image_three,
   },
 ];
 
@@ -51,7 +51,7 @@ const InstagramCarousel = () => {
     const storyDuration = 4000;
     const contentUpdateDelay = 0.4;
     let direction = "next";
-    let storyTimeout = 0;
+    let storyTimeout: number;
 
     const cursor = document.querySelector<HTMLDivElement>(".carousel-cursor");
     const cursorText = cursor!.querySelector<HTMLParagraphElement>("p");
@@ -59,7 +59,7 @@ const InstagramCarousel = () => {
     const currentImageContainer = document.querySelector<HTMLDivElement>(".carousel-image-wrapper .carousel-image");
     const currentImage = currentImageContainer!.querySelector<HTMLImageElement>("img");
     const profileNameContainer = document.querySelector<HTMLDivElement>(".carousel-profile-name");
-    const profileImage = document.querySelector<HTMLImageElement>(".carousel-profile-image img");
+    const profileImage = document.querySelector<HTMLImageElement>(".carousel-profile-icon img");
     const titleRows = document.querySelectorAll<HTMLDivElement>(".carousel-content-title-row");
     const link = document.querySelector<HTMLAnchorElement>(".carousel-content-link a");
     const highlight = document.querySelectorAll<HTMLDivElement>(".carousel-index .carousel-index-highlight");
@@ -88,6 +88,7 @@ const InstagramCarousel = () => {
 
       setTimeout(() => {
         const newProfileName = document.createElement("p");
+        newProfileName.classList.add("text-white");
         newProfileName.innerText = story.profileName;
         newProfileName.style.transform = direction === "next" ? "translateY(24px)" : "translateY(-24px)";
         profileNameContainer!.appendChild(newProfileName);
@@ -101,6 +102,7 @@ const InstagramCarousel = () => {
         story.title.forEach((line, index) => {
           if (titleRows[index]) {
             const newTitle = document.createElement("h2");
+            newTitle.classList.add("absolute", "top-0", "text-white", "text-[2.25rem]");
             newTitle.innerText = line;
             newTitle.style.transform = direction === "next" ? "translateY(48px)" : "translateY(-48px)";
             titleRows[index].appendChild(newTitle);
@@ -114,9 +116,10 @@ const InstagramCarousel = () => {
         });
 
         const newImageContainer = document.createElement("div");
-        newImageContainer.classList.add("carousel-image");
+        newImageContainer.classList.add("carousel-image", "absolute", "top-0", "left-0", "h-full", "w-full");
         const newImage = document.createElement("img");
-        newImage.src = story.storyImage;
+        newImage.classList.add("absolute", "top-0", "left-0", "h-full", "w-full", "object-cover");
+        newImage.src = story.imageSrc;
         newImage.alt = story.profileName;
         newImageContainer.appendChild(newImage);
         imageContainer!.appendChild(newImageContainer);
@@ -135,9 +138,9 @@ const InstagramCarousel = () => {
       }, 200);
 
       setTimeout(() => {
-        profileImage!.src = story.profileImage;
+        profileImage!.src = story.profileIconSrc;
         link!.textContent = story.linkLabel;
-        link!.href = story.link;
+        link!.href = story.linkSrc;
       }, 600);
     }
 
@@ -178,16 +181,13 @@ const InstagramCarousel = () => {
         upcomingImage,
         {
           scale: 2,
-          rotate: direction === "next" ? -25 : 25,
+          rotate: direction === "next" ? 25 : -25,
         },
         {
           scale: 1,
           rotate: 0,
           duration: 1,
           ease: "power4.inOut",
-          onComplete: () => {
-            currentImage!.parentElement!.remove();
-          },
         },
       );
     }
@@ -209,6 +209,11 @@ const InstagramCarousel = () => {
 
     function animateIndexHighlight(index: number) {
       gsap.set(highlight[index], {
+        width: "0%",
+        scaleX: 1,
+        transformOrigin: "right center",
+      });
+      gsap.to(highlight[index], {
         width: "100%",
         duration: storyDuration / 1000,
         ease: "none",
@@ -259,117 +264,66 @@ const InstagramCarousel = () => {
 
   return (
     <>
-      <div className="carousel-container relative">
-        <div className="carousel-cursor">
-          <p></p>
+      <div className="carousel-container relative bg-black h-svh w-full cursor-none overflow-hidden">
+        <div
+          className={cn(
+            "carousel-cursor rounded-full bg-white/5 absolute top-0 left-0 size-20 flex",
+            "justify-center items-center backdrop-blur-[1.25rem] pointer-event-none z-2",
+          )}
+        >
+          <p className="text-[0.725rem] text-white uppercase"></p>
         </div>
 
-        <div className="carousel-image-wrapper">
-          <div className="carousel-image">
-            <img src={image_two} alt="" />
+        <div className="carousel-image-wrapper absolute top-0 left-0 h-svh w-full opacity-50 overflow-hidden">
+          <div className="carousel-image absolute top-0 left-0 h-full w-full">
+            <img className="absolute top-0 left-0 h-full w-full object-cover" src={image_two} alt="" />
           </div>
         </div>
 
-        <div className="carousel-content">
+        <div className="carousel-content absolute top-1/2 left-1/2 -translate-1/2 h-full w-[30%] py-[4em] flex flex-col justify-between">
           <div className="carousel-content-row">
-            <div className="carousel-indices">
-              <div className="carousel-index">
-                <div className="carousel-index-highlight"></div>
-              </div>
-              <div className="indexcarousel-">
-                <div className="carousel-index-highlight"></div>
-              </div>
-              <div className="indexcarousel-">
-                <div className="carousel-index-highlight"></div>
-              </div>
+            <div className="carousel-indices h-px w-full flex justify-between items-center gap-[0.25em]">
+              {stories.map((story) => (
+                <div className="carousel-index bg-white/25 relative h-px w-full" key={story.imageSrc}>
+                  <div className="carousel-index-highlight bg-white absolute top-0 left-0 h-full w-0 scale-x-100"></div>
+                </div>
+              ))}
             </div>
 
-            <div className="carousel-profile">
-              <div className="carousel-profile-icon">
-                <img src={logo_guto} alt="" />
+            <div className="carousel-profile h-15 w-full flex items-center gap-[1em]">
+              <div className="carousel-profile-icon relative rounded-full size-10 overflow-hidden">
+                <img className="absolute top-0 left-0 h-full w-full object-cover" src={logo_guto} alt="" />
               </div>
-              <div className="carousel-profile-name">
-                <p></p>
+              <div className="carousel-profile-name relative h-5 w-full [clip-path:polygon(0%_0%,100%_0%,100%_100%,0%_100%)]">
+                <p className="text-white"></p>
               </div>
             </div>
           </div>
 
           <div className="carousel-content-row">
             <div className="carousel-content-title">
-              <div className="carousel-content-title-row">
-                <h2>Sua criatividade</h2>
+              <div className="carousel-content-title-row relative h-12 w-full [clip-path:polygon(0%_0%,100%_0%,100%_100%,0%_100%)]">
+                <h2 className="absolute top-0 text-[2.25rem] text-white no-underline">Sua criatividade</h2>
               </div>
-              <div className="carousel-content-title-row">
-                <h2>não terá limites</h2>
+              <div className="carousel-content-title-row relative h-12 w-full [clip-path:polygon(0%_0%,100%_0%,100%_100%,0%_100%)]">
+                <h2 className="absolute top-0 text-[2.25rem] text-white no-underline">não terá limites</h2>
               </div>
-              <div className="carousel-content-title-row">
-                <h2>se você ousar</h2>
+              <div className="carousel-content-title-row relative h-12 w-full [clip-path:polygon(0%_0%,100%_0%,100%_100%,0%_100%)]">
+                <h2 className="absolute top-0 text-[2.25rem] text-white no-underline">se você ousar</h2>
               </div>
             </div>
-            <div className="carousel-content-link">
-              <a href=""> Leia mais</a>
+            <div className={cn(
+                "carousel-content-link relative w-max my-[2em] py-[0.25em] after:content-['']",
+                "after:bg-white after:absolute after:top-full after:left-0 after:h-px after:w-full"
+            )}>
+              <a className="text-white no-underline" href="">
+                {" "}
+                Leia mais
+              </a>
             </div>
           </div>
         </div>
       </div>
-
-      {/* <div className="carousel-container relative h-svh w-ful overflow-hidden cursor-none">
-                <div className={cn(
-                    "cursor rounded-full bg-white/5 backdrop-blur-xl absolute top-0 left-0",
-                    "size-25 grid place-content-center pointer-events-none z-2"
-                )}>
-                    <p className="text-xs uppercase"></p>
-                </div>
-
-                <div className="story-image-wrapper absolute top-0 left-0 h-svh w-svw mx-auto overflow-hidden opacity-85">
-                    <div className="story-img absolute top-0 left-0 h-full w-full">
-                        <img className="absolute top-0 left-0 h-full w-full object-cover" src={image_two} alt="" />
-                    </div>
-                </div>
-
-                <div className="story-content-wrapper absolute top-1/2 left-1/2 -translate-1/2 h-full w-[30%] py-[4em] flex flex-col justify-between">
-                    <div className="story-content-row">
-                        <div className="story-indices h-[10%] w-full flex justify-between items-center gap-1">
-                            <div className="story-index relative h-px w-full bg-white/25">
-                                <div className="story-index-highlight bg-white absolute top-0 left-0 h-full w-0 scale-100"></div>
-                            </div>
-                            <div className="story-index relative h-px w-full bg-white/25">
-                                <div className="story-index-highlight bg-white absolute top-0 left-0 h-full w-0 scale-100"></div>
-                            </div>
-                            <div className="story-index relative h-px w-full bg-white/25">
-                                <div className="story-index-highlight bg-white absolute top-0 left-0 h-full w-0 scale-100"></div>
-                            </div>
-                        </div>
-                        <div className="profile h-15 w-full flex items-center gap-4">
-                            <div className="profile-icon rounded-full relative size-10 overflow-hidden">
-                                <img className="absolute top-0 left-0 h-full w-full object-cover" src={logo_guto} alt="" />
-                            </div>
-                            <div className="profile-name relative h-5 w-50 [clip-path:polygon(0%_0%,100%_0%,100%_100%,0%_100%)]">
-                                <p className="absolute top-0 text-white">Guto Moraes</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="story-content-row">
-                        <div className="story-title text-white">
-                            <div className="story-title-row relative h-10.5 w-full [clip-path:polygon(0%_0%,100%_0%,100%_100%,0%_100%)]">
-                                <h2 className="absolute top-0 text-4xl">Uma Capela que</h2>
-                            </div>
-                            <div className="story-title-row relative h-10.5 w-full [clip-path:polygon(0%_0%,100%_0%,100%_100%,0%_100%)]">
-                                <h2 className="absolute top-0 text-4xl">transformou-se em</h2>
-                            </div>
-                            <div className="story-title-row relative h-10.5 w-full [clip-path:polygon(0%_0%,100%_0%,100%_100%,0%_100%)]">
-                                <h2 className="absolute top-0 text-4xl">um patrimônio</h2>
-                            </div>
-                        </div>
-                        <div className={cn(
-                            "story-content-link relative w-max my-[2em] py-[0.25em]",
-                            "after:content-[''] after:bg-white after:absolute after:top-full after:left-0 after:h-px after:w-full"
-                        )}>
-                            <a href="/" title="">Saiba mais</a>
-                        </div>
-                    </div>
-                </div>
-            </div> */}
     </>
   );
 };
